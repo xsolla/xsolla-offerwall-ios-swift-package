0d77c33adb07ab39e8a917b10c4c350d628ce8c7
Dominic Roberts <d.roberts@xsolla.com>
2026-05-26 10:55:37 +0100

    Removed 0.1.0 XCFramework

--- XsollaOfferwallSDK.xcframework/ios-arm64_x86_64-simulator/XsollaOfferwallSDK.framework/Headers/XsollaOfferwallSDK.h
+++
@@ -1,4 +0,0 @@
-// Import Swift-generated Objective-C header to expose Swift classes to Objective-C
-#if __has_include(<XsollaOfferwallSDK/XsollaOfferwallSDK-Swift.h>)
-#import <XsollaOfferwallSDK/XsollaOfferwallSDK-Swift.h>
-#endif
